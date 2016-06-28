78d1efd27724eddc7d6aec8579a8fccb227bd48d
benhassara <bhassara@gmail.com>
2016-06-28 16:39:19 -0600

    get a guess from the player

--- hangman.js
+++ hangman.js
@@ -3,10 +3,12 @@
 var word = pickWord(wordList);
 // creates a string of the same length as word, but each char is an underscore
 var maskedWord = mask(word);
+var guess = getPlayerGuess();
 
 console.log(word);
 console.log(maskedWord);
 console.log(displayScore(maskedWord));
+console.log(guess);
 
 // chooses a random word from an array of words
 function pickWord(wordArray) {
@@ -25,7 +27,26 @@ function mask(word) {
 
 // display the game progress to the player
 function displayScore(maskedWord) {
-  // join the masked array into a showable string
+  // join the masked array into a readable string
   return maskedWord.join(' ');
+}
+
+// asks the player to guess a letter
+// if they click Cancel, this will end the game
+function getPlayerGuess() {
+  var guess = prompt('Guess a letter, or click Cancel to quit.');
+
+  // guess will be null when they click Cancel instead of entering a char
+  if (guess === null) {
+    return null;
+  }
+  // we only want single letter guesses
+  else if (guess.length !== 1) {
+    alert('Please enter a single letter.');
+  }
+  // if it's just a single letter, we're good to go
+  else {
+    return guess.toLowerCase();
+  }
 }
 
